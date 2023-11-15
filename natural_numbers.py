5535465fff58c5c5eb074cb761daa93c3bd9d36e
Guffy <144727889+Greatcthulhu13@users.noreply.github.com>
2023-11-15 11:05:25 +0000

    Add files via upload

---
+++ natural_numbers.py
@@ -0,0 +1,10 @@
+def sum_of_natural_numbers(n):
+    return (n * (n + 1) // 2
+
+num = int(input("Enter a positive integer: "))
+
+if num < 0:
+    print("Please enter a postive integer.")
+else:
+    result = sum_of_natural_numbers(num)
+    print(f"The sum of natural numbers up tp {num} is: {result}")
